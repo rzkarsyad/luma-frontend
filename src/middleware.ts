9102398194ca019b2ec129f9cd25369bbf8e16f7
Rizki Arsyad <arsyadrizki16@gmail.com>
2024-07-07 22:50:00 +0700

    update middleware - disabled root endpoint while logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,12 +4,17 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('jwt_token')?.value;
 
+  if (token && request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/chat', request.url));
+  }
+
   if (!token && request.nextUrl.pathname === '/chat') {
     return NextResponse.redirect(new URL('/', request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/chat'],
+  matcher: ['/', '/chat'],
 };
